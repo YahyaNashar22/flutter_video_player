e5fdbd6d9a1365bb28aba49489eb73a32b8fab9d
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-15 15:04:22 +0200

    fixed issue with aspect ratio | fixed issue with alert showing multiple tims

--- lib/video_player_screen.dart
+++ lib/video_player_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:chewie/chewie.dart';
 import 'package:video_player/video_player.dart';
+import 'package:image_picker/image_picker.dart';
 
 class VideoPlayerScreen extends StatefulWidget {
   const VideoPlayerScreen({super.key});
@@ -11,40 +14,58 @@ class VideoPlayerScreen extends StatefulWidget {
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late VideoPlayerController _videoPlayerController;
-  late ChewieController _chewieController;
+  ChewieController? _chewieController;
+
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
     super.initState();
-    _videoPlayerController = VideoPlayerController.asset('assets/cmd.mp4')
-      ..addListener(() {
-        if (_videoPlayerController.value.position ==
-            _videoPlayerController.value.duration) {
-          _showAlert();
-        }
-      })
-      ..initialize().then((_) {
-        setState(() {});
-      });
+  }
 
-    _chewieController = ChewieController(
-      videoPlayerController: _videoPlayerController,
-      aspectRatio: 16 / 9,
-      autoPlay: true,
-      looping: false,
-    );
+  Future<void> _pickVideo() async {
+    final XFile? pickedFile =
+        await _picker.pickVideo(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      // If there's a previous controller, dispose it to remove listeners
+      if (_videoPlayerController.value.isInitialized) {
+        _videoPlayerController.removeListener(() {});
+      }
+
+      _videoPlayerController = VideoPlayerController.file(File(pickedFile.path))
+        ..addListener(() {
+          if (_videoPlayerController.value.duration ==
+              _videoPlayerController.value.position) {
+            _showAlert();
+          }
+        })
+        ..initialize().then((_) {
+          setState(() {
+            _chewieController = ChewieController(
+              videoPlayerController: _videoPlayerController,
+              looping: false,
+              allowFullScreen: true,
+              fullScreenByDefault: false,
+            );
+          });
+        });
+    }
   }
 
   void _showAlert() {
     showDialog(
         context: context,
         builder: (context) => AlertDialog(
-              title: const Text("Hire me!"),
-              content: const Text("Don't be biased from previous experiences!"),
+              title: const Text("Video Ended!"),
+              content: const Text("Hope you enjoyed it..."),
               actions: [
                 TextButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    child: const Text("You are hired!"))
+                    onPressed: () {
+                      _videoPlayerController.seekTo(Duration.zero);
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Ok"))
               ],
             ));
   }
@@ -52,7 +73,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   @override
   void dispose() {
     _videoPlayerController.dispose();
-    _chewieController.dispose();
+    _chewieController?.dispose();
     super.dispose();
   }
 
@@ -61,14 +82,18 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Video Player"),
+        actions: [
+          IconButton(
+              onPressed: _pickVideo, icon: const Icon(Icons.video_library)),
+        ],
       ),
       body: Center(
         child: _chewieController != null &&
-                _chewieController.videoPlayerController.value.isInitialized
+                _chewieController!.videoPlayerController.value.isInitialized
             ? Chewie(
-                controller: _chewieController,
+                controller: _chewieController!,
               )
-            : const CircularProgressIndicator(),
+            : const Text("Pick A Video!"),
       ),
     );
   }
